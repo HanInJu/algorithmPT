9503c0298d0c4d8053a2616f06fe388990b43ece
HanInJu <haninju95@gmail.com>
2022-01-05 20:57:06 +0900

    w2-2:Q3

--- src/w2/Q3.java
+++ src/w2/Q3.java
@@ -1,8 +1,44 @@
 package w2;
 
-public class Q3 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
+public class Q3 { // https://www.acmicpc.net/problem/5397
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] testCase = new String[n];
+        for(int i = 0; i<n; i++) {
+            testCase[i] = br.readLine();
+        }
+
+        int cur = 0, k = 0;
+        List<Character> password = new ArrayList<>();
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<testCase[i].length(); i++) {
+                switch(testCase[i].charAt(j)) {
+                    case '<':
+                        if(cur != 0) {
+
+                        }
+                    case '>':
+
+                    case'-':
+                        if((0 < cur) && (cur <= k)) {
+                            password.remove(--cur);
+                        }
+
+                        continue;
+                    default:
+                        k++;
+                        password.add(cur, testCase[i].charAt(j));
+                }
+            }
+        }
     }
 }
