fea7034ee09ee0c706a0e7ad25ec2b97befd03bb
HanInJu <haninju95@gmail.com>
2022-01-08 22:56:57 +0900

    W2:Q4,Q5

--- src/w2/Q4.java
+++ src/w2/Q4.java
@@ -1,8 +1,31 @@
 package w2;
 
-public class Q4 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) {
+public class Q4 { // https://www.acmicpc.net/problem/14425
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> setA = new HashSet<>(n);
+        for(int i = 0; i<n; i++) {
+            setA.add(br.readLine());
+        }
+
+        int cnt = 0;
+        for(int i = 0; i<m; i++) {
+            if(setA.contains(br.readLine())) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
--- src/w2/Q5.java
+++ src/w2/Q5.java
@@ -1,8 +1,36 @@
 package w2;
 
-public class Q5 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-    public static void main(String[] args) {
+public class Q5 { // https://www.acmicpc.net/problem/2841
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        Stack[] melody = new Stack[6];
+        for(int i = 0; i<6; i++) {
+            melody[i] = new Stack<Integer>();
+        }
+
+        for(int i = 0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int line = Integer.parseInt(st.nextToken());
+            int prat = Integer.parseInt(st.nextToken());
+
+            int idx;
+            for(int j = 0; j<melody[line-1].size(); j++) {
+                if((Integer) melody[line-1].get(j) == prat) {
+                    idx = melody[line-1].indexOf(prat);
+                }
+            }
+            idx = -1;
+
+        }
     }
 }
