0727d457370af4cb74651d55492e8abf11f6d23a
HanInJu <haninju95@gmail.com>
2022-03-01 00:15:29 +0900

    [W6] 준비

---
+++ src/w5/Q9.java
@@ -0,0 +1,8 @@
+package w5;
+
+public class Q9 { // https://www.acmicpc.net/problem/2468
+
+	public static void main(String[] args) {
+
+	}
+}
--- src/w6/Q1.java
+++ src/w6/Q1.java
@@ -1,6 +1,6 @@
 package w6;
 
-public class Q1 {
+public class Q1 { // https://www.acmicpc.net/problem/1260
     public static void main(String[] args) {
 
     }
